36453411c3b2776723ae01e6576ab76b0e60252a
Richard Essuman <ressuman001@gmail.com>
2024-10-22 20:44:59 +0000

    "Remove favicon, update globals.css, page.tsx, and NavBar.tsx components"

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function HomePage() {
   return (
     <>
-      <UserButton />
       <h2>Hi👋, welcome</h2>
     </>
   );
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,3 +1,33 @@
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
 export default function NavBar() {
-  return <div></div>;
+  return (
+    <div className="flex justify-between p-3 px-10 border-b-[1px] shadow-md">
+      <div className="flex gap-10 items-center">
+        <Image src="/logo.png" alt="logo" width={120} height={60} priority />
+        <div className="hidden md:flex gap-6 ">
+          <h2
+            className="hover:bg-gray-100 p-2
+                rounded-md cursor-pointer transition-all"
+          >
+            Home
+          </h2>
+          <h2
+            className="hover:bg-gray-100 p-2
+                rounded-md cursor-pointer transition-all"
+          >
+            History
+          </h2>
+          <h2
+            className="hover:bg-gray-100 p-2
+                rounded-md cursor-pointer transition-all"
+          >
+            Help
+          </h2>
+        </div>
+      </div>
+      <UserButton />
+    </div>
+  );
 }
