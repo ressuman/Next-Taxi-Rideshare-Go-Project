e7ea34dfcd1f6562f9f1ce70b539d13428beab54
Richard Essuman <ressuman001@gmail.com>
2024-10-22 15:34:30 +0000

    "Added NavBar component to RootLayout and removed Image import from HomePage"

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import NavBar from "@/components/NavBar";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -22,7 +23,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={outfit.className}>{children}</body>
+        <body className={outfit.className}>
+          <NavBar />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
