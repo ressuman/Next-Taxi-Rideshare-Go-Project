8e8da9e9617c0262b20d6ff0dbdb6b248c293101
Richard Essuman <ressuman001@gmail.com>
2024-10-25 11:10:42 +0000

    "Added 'use client' directive, user location context, and geolocation functionality to HomePage component"

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,48 @@
+"use client";
+
 import Booking from "@/components/Booking/booking";
 import MapboxMap from "@/components/Map/mapbox-map";
+import { UserLocationContext } from "@/context/user-location-context";
+import { useEffect, useState } from "react";
+
+interface Location {
+  lat: number;
+  lng: number;
+}
 
 export default function HomePage() {
+  const [userLocation, setUserLocation] = useState<Location | null>(null);
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  function getUserLocation(): void {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      setUserLocation({
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      });
+    });
+  }
+
   return (
     <div className="">
-      <div className="grid grid-cols-1 md:grid-cols-3">
-        <div className="">
-          <Booking />
-        </div>
+      <UserLocationContext.Provider value={{ userLocation, setUserLocation }}>
+        {" "}
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          <div className="">
+            <Booking />
+          </div>
 
-        <div
-          className="col-span-2
+          <div
+            className="col-span-2
         "
-        >
-          <MapboxMap />
+          >
+            <MapboxMap />
+          </div>
         </div>
-      </div>
+      </UserLocationContext.Provider>
     </div>
   );
 }
