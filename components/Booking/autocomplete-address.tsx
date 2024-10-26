bd019c3a7608ab601374420631b068c76b24f33a
Richard Essuman <ressuman001@gmail.com>
2024-10-26 16:06:20 +0000

    "Added source and destination coordinates state variables and implemented onSourceAddressClick and onDestinationAddressClick functions to fetch coordinates from Mapbox API"

--- components/Booking/autocomplete-address.tsx
+++ components/Booking/autocomplete-address.tsx
@@ -6,6 +6,9 @@ export default function AutocompleteAddress() {
   const [destinationChange, setDestinationChange] = useState<any>(false);
   const [addressList, setAddressList] = useState<any>([]);
   const [destination, setDestination] = useState<any>();
+  const [sourceCoordinates, setSourceCoordinates] = useStateuseState<any>([]);
+  const [destinationCoordinates, setDestinationCoordinates] =
+    useStateuseState<any>([]);
 
   const getAddressList = async () => {
     setAddressList([]);
@@ -26,6 +29,56 @@ export default function AutocompleteAddress() {
     }, 1000);
     return () => clearTimeout(delayDebounceFn);
   }, [source, destination]);
+
+  const onSourceAddressClick = async (suggestion: any) => {
+    setSource(suggestion.full_address);
+    setAddressList([]);
+    setSourceChange(false);
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_MAPBOX_BASE_RETRIEVE_URL}/${suggestion.mapbox_id}?session_token=${process.env.NEXT_PUBLIC_MAPBOX_SESSION_TOKEN}&access_token=${process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_ACCESS_TOKEN}`
+      );
+
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+
+      const result = await res.json();
+
+      setSourceCoordinates({
+        lng: result.features[0].geometry.coordinates[0],
+        lat: result.features[0].geometry.coordinates[1],
+      });
+      console.log(result);
+    } catch (error) {
+      console.error("Error fetching data from Mapbox:", error);
+    }
+  };
+
+  const onDestinationAddressClick = async (suggestion: any) => {
+    setDestination(suggestion.full_address);
+    setAddressList([]);
+    setDestinationChange(false);
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_MAPBOX_BASE_RETRIEVE_URL}/${suggestion.mapbox_id}?session_token=${process.env.NEXT_PUBLIC_MAPBOX_SESSION_TOKEN}&access_token=${process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_ACCESS_TOKEN}`
+      );
+
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+
+      const result = await res.json();
+
+      setDestinationCoordinates({
+        lng: result.features[0].geometry.coordinates[0],
+        lat: result.features[0].geometry.coordinates[1],
+      });
+      console.log(result);
+    } catch (error) {
+      console.error("Error fetching data from Mapbox:", error);
+    }
+  };
 
   return (
     <form className="mt-5">
@@ -53,9 +106,7 @@ export default function AutocompleteAddress() {
                 className="p-3 hover:bg-gray-100
                 cursor-pointer"
                 onClick={() => {
-                  setSource(suggestion.full_address);
-                  setAddressList([]);
-                  setSourceChange(false);
+                  onSourceAddressClick(suggestion);
                 }}
               >
                 {suggestion.full_address}
@@ -92,9 +143,7 @@ export default function AutocompleteAddress() {
                 className="p-3 hover:bg-gray-100
                 cursor-pointer"
                 onClick={() => {
-                  setDestination(suggestion.full_address);
-                  setAddressList([]);
-                  setDestinationChange(false);
+                  onDestinationAddressClick(suggestion);
                 }}
               >
                 {suggestion.full_address}
