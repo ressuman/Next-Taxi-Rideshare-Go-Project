bd9189a7fb82e4f0c23da7f7d73abf9ca38ebd58
Richard Essuman <ressuman001@gmail.com>
2024-10-29 16:56:52 +0000

    "Refactor payment page and check-out form components to integrate Stripe payment elements"

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,19 +1,17 @@
+"use client";
+
 import CheckOutForm from "@/components/Payment/check-out-form";
-import { SelectedCarAmountContext } from "@/context/selected-car-amount-context";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import { useContext } from "react";
 
 export default function PaymentPage() {
-  const { carAmount, setCarAmount } = useContext(SelectedCarAmountContext);
-
   const stripePromise = loadStripe(
     process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
   );
 
   const options: any = {
     mode: "payment",
-    amount: carAmount,
+    amount: 1059,
     currency: "usd",
   };
 
--- components/Payment/check-out-form.tsx
+++ components/Payment/check-out-form.tsx
@@ -1,3 +1,53 @@
+import {
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { useState } from "react";
+
 export default function CheckOutForm() {
-  return <div></div>;
+  const stripe: any = useStripe();
+  const elements: any = useElements();
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (elements == null || stripe == null) {
+      return;
+    }
+
+    // Trigger form validation and wallet collection
+    const { error: submitError } = await elements.submit();
+    if (submitError?.message) {
+      // Show error to your customer
+      setErrorMessage(submitError.message);
+      return;
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="px-4">
+      <div className="mb-3">
+        <label htmlFor="email-input">Email</label>
+        <div>
+          <input
+            value={emailInput}
+            onChange={(e) => setEmailInput(e.target.value)}
+            type="email"
+            id="email-input"
+            placeholder="johndoe@gmail.com"
+          />
+        </div>
+      </div>
+      <PaymentElement />
+      <button type="submit" disabled={!stripe || !elements}>
+        Pay
+      </button>
+      {/* Show error message to your customers */}
+      {errorMessage && <div>{errorMessage}</div>}
+    </form>
+  );
 }
