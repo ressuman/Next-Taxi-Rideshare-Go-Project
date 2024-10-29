e5ef1fc3e1d277fabe9ae68c1e256b7c079e078f
Richard Essuman <ressuman001@gmail.com>
2024-10-29 19:36:46 +0000

    "Updated CheckOutForm component to create PaymentIntent and obtain clientSecret from server endpoint, and modified form layout and styling."

--- components/Payment/check-out-form.tsx
+++ components/Payment/check-out-form.tsx
@@ -26,28 +26,60 @@ export default function CheckOutForm() {
       setErrorMessage(submitError.message);
       return;
     }
+
+    // Create the PaymentIntent and obtain clientSecret from your server endpoint
+    const res = await fetch("/api/create-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        currency: "usd",
+        email: emailInput,
+        amount: 100,
+        paymentMethodType: "card",
+      }),
+    });
+
+    const { client_secret: clientSecret } = await res.json();
+    console.log(clientSecret);
+
+    const { error } = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      clientSecret,
+      confirmParams: {
+        return_url: "http://localhost:3000",
+      },
+    });
   };
 
   return (
-    <form onSubmit={handleSubmit} className="px-4">
-      <div className="mb-3">
-        <label htmlFor="email-input">Email</label>
-        <div>
-          <input
-            value={emailInput}
-            onChange={(e) => setEmailInput(e.target.value)}
-            type="email"
-            id="email-input"
-            placeholder="johndoe@gmail.com"
-          />
+    <div className="flex flex-col items-center justify-center w-full mt-6">
+      <form onSubmit={handleSubmit} className="max-w-lg">
+        <div className="mb-3">
+          <label htmlFor="email-input">Email</label>
+          <div>
+            <input
+              value={emailInput}
+              onChange={(e) => setEmailInput(e.target.value)}
+              type="email"
+              id="email-input"
+              placeholder="johndoe@gmail.com"
+            />
+          </div>
         </div>
-      </div>
-      <PaymentElement />
-      <button type="submit" disabled={!stripe || !elements}>
-        Pay
-      </button>
-      {/* Show error message to your customers */}
-      {errorMessage && <div>{errorMessage}</div>}
-    </form>
+        <PaymentElement />
+        <button
+          type="submit"
+          disabled={!stripe || !elements}
+          className="w-full bg-yellow-500 p-2 rounded-lg mt-2"
+        >
+          Pay
+        </button>
+        {/* Show error message to your customers */}
+        {/* {errorMessage && <div>{errorMessage}</div>} */}
+      </form>
+    </div>
   );
 }
