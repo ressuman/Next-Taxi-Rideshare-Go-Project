a8ade50f04d31190ec96a52181111306360c1951
Richard Essuman <ressuman001@gmail.com>
2024-11-01 20:10:37 +0000

    Remove commented autoCert integration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+//import autoCert from "anchor-pki/auto-cert/integrations/next";
+
 const nextConfig = {};
 
+// const withAutoCert = autoCert({
+//   enabledEnv: "development",
+// });
+
 export default nextConfig;
+//export default withAutoCert(nextConfig);
